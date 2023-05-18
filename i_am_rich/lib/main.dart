5f3881704cfb3d4aadd536d4516360fbc0cfcb61
jaz2000 <jazeelanwar@gmail.com>
2023-05-18 10:04:32 +0530

    scaffold appbar image

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home :Center(
-        child: Text("Hello World"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('I am rich')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA9RF9xGDZJzt3hfNnMIwTfGefBA5NjUmb0KEi1h-c5w&ec=48600113')),
+        ),
+        backgroundColor: Colors.blueGrey,
       ),
     ),
   );
