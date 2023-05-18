7ebdc6de8537a5fa736d569db60d2a48652fd6cd
jaz2000 <jazeelanwar@gmail.com>
2023-05-19 00:32:50 +0530

    statefull widget

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,7 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Center(child: Text('I am rich')),
@@ -15,6 +29,6 @@ void main() {
         ),
         backgroundColor: Colors.blueGrey,
       ),
-    ),
-  );
+    );
+  }
 }
