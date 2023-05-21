005742f921262a3aa7208c74ddd66f3ecda1a5f1
jaz2000 <jazeelanwar@gmail.com>
2023-05-21 20:44:58 +0530

    started dicee flutter project

---
+++ dicee_flutter/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Image.asset("images/dice1.png"),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Image.asset("images/dice1.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
