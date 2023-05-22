29af31e9da7a3ff54ecb560509285c626ae059fe
jaz2000 <jazeelanwar@gmail.com>
2023-05-22 18:52:07 +0530

    started xylophone app

---
+++ xylophone_flutter/android/app/src/main/java/co/appbrewery/xylophone/MainActivity.java
@@ -0,0 +1,10 @@
+package co.appbrewery.xylophone;
+
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
+public class MainActivity extends FlutterActivity {
+
+
+}
