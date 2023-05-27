9e59303741ffadcc8a22cfe6024470a8b0bd7676
jaz2000 <jazeelanwar@gmail.com>
2023-05-27 20:17:10 +0530

    new knowledege of hiding widget using visibility widget

--- destini_challenge_starting/lib/story.dart
+++ destini_challenge_starting/lib/story.dart
@@ -1,4 +1,8 @@
 class Story {
-  String storyTitle, choice1, choice2;
-  Story(this.storyTitle, this.choice1, this.choice2);
+  late String storyTitle, choice1, choice2;
+  Story({storyTitle, choice1, choice2}) {
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+  }
 }
