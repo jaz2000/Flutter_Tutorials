f60ab6d11d037a92e7ef291d9548130f504a6353
jaz2000 <jazeelanwar@gmail.com>
2023-05-28 16:46:53 +0530

    refactored widgets

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -11,12 +12,65 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xff1D1E33),
+                  ),
+                ),
+                // SizedBox(width: 10),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xff1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // SizedBox(height: 10),
+          Expanded(
+            child: ReusableCard(
+              colour: Color(0xff1D1E33),
+            ),
+          ),
+          // SizedBox(height: 10),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xff1D1E33),
+                  ),
+                ),
+                // SizedBox(width: 10),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xff1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color colour;
+  ReusableCard({required this.colour});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
