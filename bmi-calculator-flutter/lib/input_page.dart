091001d6336f0f26b77b4a71d7bb0114ba568fc8
jaz2000 <jazeelanwar@gmail.com>
2023-05-28 17:58:13 +0530

    fixed errors

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xff1D1E33);
@@ -24,6 +25,24 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     colour: activeCardColor,
+                    cardWidget: Column(
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          'MALE',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xFF8D8E98),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 // SizedBox(width: 10),
@@ -73,8 +92,8 @@ class _InputPageState extends State<InputPage> {
 
 class ReusableCard extends StatelessWidget {
   final Color colour;
-  // final Widget cardWidget;
-  ReusableCard({required this.colour});
+  final Widget? cardWidget;
+  ReusableCard({required this.colour, this.cardWidget});
   @override
   Widget build(BuildContext context) {
     return Container(
