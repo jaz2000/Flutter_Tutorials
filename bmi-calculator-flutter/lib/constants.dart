44a21edd4c6b1ec3fc7d9239382c0d6766f1b14c
jaz2000 <jazeelanwar@gmail.com>
2023-05-28 22:45:06 +0530

    created new file constants.dart

---
+++ bmi-calculator-flutter/lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xff1D1E33);
+const inactiveCardColor = Color(0xff111328);
+const bottomContaierColor = Color(0xFFEB1555);
+const labelTextStyle = TextStyle(
+  fontSize: 18,
+  color: Color(0xFF8D8E98),
+);
--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'constants.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
 
-const bottomContainerHeight = 80.0;
-const activeCardColor = Color(0xff1D1E33);
-const inactiveCardColor = Color(0xff111328);
-const bottomContaierColor = Color(0xFFEB1555);
+
 
 enum Gender { male, female }
 
